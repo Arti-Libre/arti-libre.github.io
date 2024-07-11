597840fcf988b8b3e8c8fc76f58453956707e875
sinceaway <sinceaway.contact@gmail.com>
2024-07-11 22:21:25 +0200

    fix burgermenu

---
+++ scripts/burger_menu.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var burgerMenu = document.querySelector('.burgermenue');
+    var popup = document.getElementById('popup');
+    var closeBtn = document.querySelector('.close');
+
+    burgerMenu.addEventListener('click', function() {
+        popup.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+
+    // Fermer la popup si l'utilisateur clique en dehors de la popup
+    window.addEventListener('click', function(event) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
