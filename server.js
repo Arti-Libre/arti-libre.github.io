c8d13e6169dc2d657f2bf5a8cd15d5d825831e34
Tom FOSSE <tomfosse.contact@gmail.com>
2024-08-07 12:46:21 +0200

    fix bugs

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Configuration de la connexion à la base de données MySQL
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
-    database: 'artisans'
+    password: '',
+    database: 'artisans',
+    port: 3301
 });
 
 db.connect((err) => {
@@ -17,29 +17,23 @@ db.connect((err) => {
     console.log('Connexion avec la base de données effectuée !');
 });
 
-// Configuration du moteur de template
-app.set('view engine', 'ejs'); // Définit EJS comme moteur de template
-app.set('views', path.join(__dirname, 'views')); // Définit le répertoire des vues
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, 'views')); 
 
-// Middleware pour servir les fichiers statiques (CSS, JS, images, etc.)
 app.use(express.static('public'));
 
-// Route pour afficher la page des résultats
 app.get('/results', (req, res) => {
     const searchTerm = req.query.q;
     if (!searchTerm) {
         return res.render('recherche', { results: [] });
     }
 
-    // Décomposer le terme de recherche en mots-clés
     const keywords = searchTerm.split(' ').map(keyword => `%${keyword}%`);
     
-    // Générer les conditions de recherche pour chaque mot-clé
     const conditions = keywords.map(() => 
         `(metier LIKE ? OR description LIKE ? OR nom LIKE ? OR prenom LIKE ? OR ville LIKE ?)`
     ).join(' OR ');
 
-    // Générer la clause de calcul de la pertinence (points) basée sur les correspondances
     const points = keywords.map((_, index) => `
         ((metier LIKE ? OR description LIKE ? OR nom LIKE ? OR prenom LIKE ? OR ville LIKE ?) * ${index + 1})
     `).join(' + ');
